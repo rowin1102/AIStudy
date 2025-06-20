447f5a8b37d79e309ea8a60fea773ce7f1e406fd
rowin1102 <rowin1102@naver.com>
2025-06-20 13:11:40 +0900

    2025-06-20

--- 01Numpy/02astype.py
+++ 01Numpy/02astype.py
@@ -22,16 +22,21 @@ print('array2 타입:', type(array2))
 # 실수가 포함되었으므로 float64로 출력된다.
 print('array2 dtype:', array2.dtype)
 
+# 문자열이 포함된 리스트 생성
 list3 = [1, 2, 'test']
 array3 = np.array(list3)
+# 모든 데이터가 문자열로 변환되어 출력
 print('array3 출력:', array3)
 print('array3 타입:', type(array3))
+# <U21로 출력. 유니코드 문자열 데이터 타입을 의미
 print('array3 dtype:', array3.dtype)
 
+# satype() : ndarray의 데이터 타입을 변경하는 함수
 array_int1 = np.array(list1)
 array_float1 = array_int1.astype('float64')
 print(array_float1, array_float1.dtype)
 
+# 실수형을 정수형으로 변경
 array_float2 = np.array([1.1, 2.1, 3.1])
 array_int2 = array_float2.astype('int32')
 print(array_int2, array_int2.dtype)
